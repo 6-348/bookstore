2523eb13be9aec6a4404e6f5780c034ae69933f3
M-M-dot <10175300204@stu.ecnu.edu.cn>
2019-12-29 12:26:50 +0800

    merge

--- script/test.sh
+++ script/test.sh
@@ -1,10 +1,6 @@
 #!/bin/sh
 export PATHONPATH=`pwd`
-<<<<<<< HEAD
 coverage run --timid --branch --source fe,be --concurrency=thread -m pytest fe/test/test_add_book.py
-=======
-coverage run --timid --branch --source fe,be --concurrency=thread -m pytest  fe/test/test_confirm.py
->>>>>>> origin/mjy
 coverage combine
 coverage report
 coverage html -d script/html
