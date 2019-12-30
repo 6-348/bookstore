5c04ca085fe37c1ba0bdabfb0ee7072de4593034
M-M-dot <10175300204@stu.ecnu.edu.cn>
2019-12-30 09:20:38 +0800

    nothing

--- fe/test/test_transfer_to_user.py
+++ fe/test/test_transfer_to_user.py
@@ -1,17 +1,17 @@
-import pytest
-
-from fe.test.gen_book_data import GenBook
-from fe.access.new_buyer import register_new_buyer
-
-import uuid
-
-class TestTransfer:
-    @pytest.fixture(autouse=True)
-    def pre_run_initialization(self):
-        # self.seller_id = "test_new_order_seller_id_{}".format(str(uuid.uuid1()))
-        self.store_id = "test_new_order_store_id_{}".format(str(uuid.uuid1()))
-        self.buyer_id = "test_new_order_buyer_id_{}".format(str(uuid.uuid1()))
-        self.password = self.seller_id
-        self.buyer = register_new_buyer(self.buyer_id, self.password)
-        self.gen_book = GenBook(self.seller_id, self.store_id)
-        yield
+# import pytest
+#
+# from fe.test.gen_book_data import GenBook
+# from fe.access.new_buyer import register_new_buyer
+#
+# import uuid
+#
+# class TestTransfer:
+#     @pytest.fixture(autouse=True)
+#     def pre_run_initialization(self):
+#         # self.seller_id = "test_new_order_seller_id_{}".format(str(uuid.uuid1()))
+#         self.store_id = "test_new_order_store_id_{}".format(str(uuid.uuid1()))
+#         self.buyer_id = "test_new_order_buyer_id_{}".format(str(uuid.uuid1()))
+#         self.password = self.seller_id
+#         self.buyer = register_new_buyer(self.buyer_id, self.password)
+#         self.gen_book = GenBook(self.seller_id, self.store_id)
+#         yield
--- script/test.sh
+++ script/test.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 export PATHONPATH=`pwd`
-coverage run --timid --branch --source fe,be --concurrency=thread -m pytest fe/test/test_my_order.py
+coverage run --timid --branch --source fe,be --concurrency=thread -m pytest
 coverage combine
 coverage report
 coverage html -d script/html
